40ca0afddbe871d7676ceb36a99dcc5eb633b3b9
rryyy <rryguerra@gmail.com>
2018-03-05 10:10:30 +0800

    postloc v3

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -24,7 +24,7 @@ class PostsController extends Controller
     public function PostWithin(Request $request)
     {
         $search = "Olongapo City";
-        $postwithin = Post::with('user')->latest()->where('location','LIKE',"%{$search}%")->get();
+        $postwithin = Post::with('user')->latest()->where('location','LIKE','%'.{$search}.'%')->get();
         return $postwithin;
     }
     public function AddPost(Request $request)
